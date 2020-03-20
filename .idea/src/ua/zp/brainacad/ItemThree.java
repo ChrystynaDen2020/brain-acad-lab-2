adbeb778ff949eb39826fa787da200c7d2fa7173
ChrystynaDen2020 <61800810+ChrystynaDen2020@users.noreply.github.com>
2020-03-20 19:02:24 +0200

    19.03.2020

---
+++ .idea/src/ua/zp/brainacad/ItemThree.java
@@ -0,0 +1,12 @@
+package ua.zp.brainacad;
+
+/**
+ *
+ */
+
+public class ItemThree {
+    public static void main(String[] args) {
+        System.out.println("Hello\nHello");
+        System.out.println("W\tO\tR\tl\tD");
+    }
+}
---
+++ .idea/src/ua/zp/brainacad/ItemTwo.java
@@ -0,0 +1,13 @@
+package ua.zp.brainacad;
+
+/**
+ *
+ */
+
+public class ItemTwo {
+    public static void main(String[] args) {
+        System.out.println("Денисенко Кристина");
+    }
+
+
+}
